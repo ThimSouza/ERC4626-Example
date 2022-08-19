8001a6b1409e9187a096e420fd6629f38a3f5c19
Thiago Souza <thiago.m.souza1@gmail.com>
2022-08-19 03:57:36 -0300

    vault created and token address added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,4 +5,19 @@ const config: HardhatUserConfig = {
   solidity: "0.8.9",
 };
 
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9"
+      },
+      {
+        version: "0.8.0"
+      },
+      {
+        version: "0.8.16"
+      }
+    ]
+  }};
+
 export default config;
